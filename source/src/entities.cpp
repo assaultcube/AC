fefc21a9e6af82853d56275c48b35ef9bcc739e3
Lee Salzman <lsalzman@gmail.com>
2006-11-06 12:44:53 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@483 e016b1c5-ed68-44aa-a2f7-31cac88cffde

---
+++ source/src/entities.cpp
@@ -0,0 +1,351 @@
+// entities.cpp: map entity related functions (pickup etc.)
+
+#include "cube.h"
+
+vector<entity> ents;
+
+char *entmdlnames[] = 
+{
+//FIXME : fix the "pickups" infront
+	"pickups/pistolclips", "pickups/ammobox", "pickups/nades", "pickups/health", "pickups/kevlar", "pickups/akimbo",
+};
+
+int triggertime = 0;
+
+void renderent(entity &e, char *mdlname, float z, float yaw, int frame = 0, int numf = 1, int basetime = 0, float speed = 10.0f)
+{
+	rendermodel(mdlname, frame, numf, 0, 1.1f, e.x, z+S(e.x, e.y)->floor, e.y, yaw, 0, false, 1.0f, speed, 0, basetime);
+};
+
+extern void newparticle(vec &o, vec &d, int fade, int type, int tex = -1);
+
+void renderentities()
+{
+    if(lastmillis>triggertime+1000) triggertime = 0;
+    loopv(ents)
+    {
+        entity &e = ents[i];
+        if(e.type==MAPMODEL)
+        {
+            mapmodelinfo &mmi = getmminfo(e.attr2);
+            if(!&mmi) continue;
+			rendermodel(mmi.name, 0, 1, e.attr4, (float)mmi.rad, e.x, (float)S(e.x, e.y)->floor+mmi.zoff+e.attr3, e.y, (float)((e.attr1+7)-(e.attr1+7)%15), 0, false, 1.0f, 10.0f, mmi.snap);
+        }
+        else if(e.type==CTF_FLAG && m_ctf) // EDIT: AH
+        {
+            flaginfo &f = flaginfos[e.attr2];
+            if(f.state==CTFF_STOLEN && f.thief)
+            {
+                if(f.thief == player1) continue;
+                s_sprintfd(path)("pickups/flags/small_%s", rb_team_string(e.attr2));
+                mapmodelinfo mmi = {10, 4, 0, 0, path};
+                if(!&mmi) continue;
+                rendermodel(mmi.name, 0, 1, 0, 0, f.thief->o.x, f.thief->o.z+0.3f+(sin(lastmillis/100.0f)+1)/10, f.thief->o.y, lastmillis/2.5f, 0, false, 0.6f, 120.0f, mmi.snap);
+            }
+            else
+            {
+                s_sprintfd(path)("pickups/flags/%s", rb_team_string(e.attr2));
+                mapmodelinfo mmi = {10, 4, 0, 0, path};
+                if(!&mmi) continue;
+                rendermodel(mmi.name, 0, 7, 0, (float)mmi.rad, e.x, f.state==CTFF_INBASE ? (float)S(e.x, e.y)->floor : e.z, e.y, (float)((e.attr1+7)-(e.attr1+7)%15), 0, false, 1.0f, 120.0f, mmi.snap);
+            };
+        }
+        else
+        {
+            if(OUTBORD(e.x, e.y)) continue;
+            if(e.type!=CARROT)
+            {
+				if(!e.spawned) continue;
+				if(e.type<I_CLIPS || e.type>I_AKIMBO) continue;
+                renderent(e, entmdlnames[e.type-I_CLIPS], (float)(1+sin(lastmillis/100.0+e.x+e.y)/20), lastmillis/10.0f);
+            }
+			else switch(e.attr2)
+            {			
+				case 1:
+				case 3:
+					continue;
+					
+                case 2: 
+                case 0:
+					if(!e.spawned) continue;
+					renderent(e, "carrot", (float)(1+sin(lastmillis/100.0+e.x+e.y)/20), lastmillis/(e.attr2 ? 1.0f : 10.0f));
+					break;
+					
+                case 4: renderent(e, "switch2", 3,      (float)e.attr3*90, (!e.spawned && !triggertime) ? 1  : 0, (e.spawned || !triggertime) ? 1 : 2,  triggertime, 1050.0f);  break;
+                case 5: renderent(e, "switch1", -0.15f, (float)e.attr3*90, (!e.spawned && !triggertime) ? 30 : 0, (e.spawned || !triggertime) ? 1 : 30, triggertime, 35.0f); break;
+            }; 
+        };
+    };
+};
+
+itemstat itemstats[] =
+{
+	{1,	 1,   1,   S_ITEMAMMO},	  //knife dummy
+    {16, 32,  72,  S_ITEMAMMO},   //pistol
+    {14, 28,  21,  S_ITEMAMMO},   //shotgun
+    {60, 90,  90,  S_ITEMAMMO},   //subgun
+    {10, 20,  15,  S_ITEMAMMO},   //sniper
+    {40, 60,  60,  S_ITEMAMMO},   //assault
+    {2,  0,   2,   S_ITEMAMMO},   //grenade
+	{33, 100, 100, S_ITEMHEALTH}, //health
+    {50, 100, 100, S_ITEMARMOUR}, //armour
+    {16, 0,   72,  S_ITEMPUP},    //powerup
+};
+
+void baseammo(int gun) { player1->ammo[gun] = itemstats[gun].add*2; };
+// Added by Rick: baseammo for bots
+void botbaseammo(int gun, dynent *d) { d->ammo[gun] = itemstats[gun].add*2; };
+// End add
+
+// these two functions are called when the server acknowledges that you really
+// picked up the item (in multiplayer someone may grab it before you).
+
+void radditem(int i, int &v, int t)
+{
+    itemstat &is = itemstats[t];
+    ents[i].spawned = false;
+    v += is.add;
+    if(v>is.max) v = is.max;
+    playsoundc(is.sound);
+};
+
+extern void weapon(int gun);
+
+void realpickup(int n, dynent *d)
+{
+    switch(ents[n].type)
+    {
+        case I_CLIPS:  
+            radditem(n, d->ammo[1], 1); 
+            break;
+    	case I_AMMO: 
+            radditem(n, d->ammo[d->primary], d->primary); 
+            break;
+        case I_GRENADE: 
+            radditem(n, d->mag[6], 6); 
+            player1->thrownademillis = 0;
+            break;
+        case I_HEALTH:  
+            radditem(n, d->health, 7);
+            break;
+        case I_ARMOUR:
+            radditem(n, d->armour, 8);
+            //d->hasarmour = true;
+            break;
+        case I_AKIMBO:
+            d->akimbomillis = lastmillis+30000;
+	        d->mag[GUN_PISTOL] = 16;
+	        radditem(n, d->ammo[1], 9);
+	        weaponswitch(GUN_PISTOL);
+            break;
+    };
+};
+
+// these functions are called when the client touches the item
+
+void additem(int i, int &v, int spawnsec, int t)
+{
+      if(v<itemstats[t].max) 
+      {
+            addmsg(SV_ITEMPICKUP, "rii", i, spawnsec);
+            ents[i].spawned = false;
+      };
+};
+
+void pickup(int n, dynent *d)
+{
+    int np = 1;
+    loopv(players) if(players[i]) np++;
+    np = np<3 ? 4 : (np>4 ? 2 : 3);         // spawn times are dependent on number of players
+    int ammo = np*2;
+    switch(ents[n].type)
+    {
+        case I_CLIPS: 
+            additem(n, d->ammo[1], ammo, 1);
+            break;
+    	case I_AMMO: 
+            additem(n, d->ammo[d->primary], ammo, d->primary); 
+            break;
+    	case I_GRENADE: additem(n, d->mag[6], ammo, 6); break;
+        case I_HEALTH:  additem(n, d->health,  np*5, 7); break;
+
+        case I_ARMOUR:
+            additem(n, d->armour, 20, 8);
+            break;
+
+        case I_AKIMBO:
+            //additem(n, d->ammo[1], 60, 9);
+            additem(n, d->akimbo, 60, 9);
+            break;
+            
+        case CARROT:
+            ents[n].spawned = false;
+            triggertime = lastmillis;
+            trigger(ents[n].attr1, ents[n].attr2, false);  // needs to go over server for multiplayer
+            break;
+            
+        case LADDER:
+        {
+            d->onladder = true;
+            break;
+        };
+
+        // EDIT: AH
+        case CTF_FLAG:
+        {
+            int team = ents[n].attr2;
+            flaginfo &f = flaginfos[team];
+            if(f.state == CTFF_STOLEN) break;
+            else if(team == rb_team_int(player1->team) && f.state == CTFF_DROPPED) 
+            {
+                addmsg(SV_FLAGRETURN, "ri", team);
+                ents[n].spawned = false;
+            }
+            else if(team != rb_team_int(player1->team)) 
+            {
+                addmsg(SV_FLAGPICKUP, "ri", team);
+                ents[n].spawned = false;
+                f.thief = player1; // do this although we don't know if we picked the flag to avoid getting it after a possible respawn
+                f.state = CTFF_STOLEN;
+                f.pick_ack = false;
+            }
+            else if(team == rb_team_int(player1->team) && f.state == CTFF_INBASE && flaginfos[rb_opposite(team)].state == CTFF_STOLEN && flaginfos[rb_opposite(team)].thief == player1) addmsg(SV_FLAGSCORE, "ri", rb_opposite(team));
+            break;
+        };
+    };
+};
+
+void checkitems()
+{
+    if(editmode) return;
+    player1->onladder = false;
+    loopv(ents)
+    {
+        entity &e = ents[i];
+        if(e.type==NOTUSED) continue;
+        if(e.type==LADDER)
+        {
+            if(OUTBORD(e.x, e.y)) continue;
+            vec v = { e.x, e.y, player1->o.z };
+            vdist(dist1, t, player1->o, v);
+            float dist2 = player1->o.z - (S(e.x, e.y)->floor+player1->eyeheight);
+            if(dist1<1.5 && dist2<e.attr1) pickup(i, player1);
+            continue;
+        };
+        
+        if(!e.spawned) continue;
+        if(OUTBORD(e.x, e.y)) continue;
+        vec v = { e.x, e.y, S(e.x, e.y)->floor+player1->eyeheight };
+        vdist(dist, t, player1->o, v);
+        if(dist<2.5) pickup(i, player1);
+    };
+};
+
+void putitems(ucharbuf &p)            // puts items in network stream and also spawns them locally
+{
+    loopv(ents) if((ents[i].type>=I_CLIPS && ents[i].type<=I_AKIMBO) || ents[i].type==CARROT)
+    {
+		if(m_noitemsnade && ents[i].type!=I_GRENADE) continue;
+		else if(m_pistol && ents[i].type==I_AMMO) continue;
+        putint(p, i);
+        ents[i].spawned = true;
+    };
+};
+
+void resetspawns() 
+{ 	
+	loopv(ents) ents[i].spawned = false;
+	if(m_noitemsnade || m_pistol)
+		loopv(ents)
+		{
+			entity &e = ents[i];
+			if(m_noitemsnade && e.type == I_CLIPS) e.type = I_GRENADE;
+			else if(m_pistol && e.type==I_AMMO) e.type = I_CLIPS;
+		}
+};
+void setspawn(int i, bool on) { if(i<ents.length() && i>=0) ents[i].spawned = on; };
+
+void radd(dynent *d)
+{
+    loopi(NUMGUNS) if(d->nextprimary!=i) d->ammo[i] = 0;
+    
+	d->mag[GUN_KNIFE] = d->ammo[GUN_KNIFE] = 1;
+    d->mag[GUN_GRENADE] = d->ammo[GUN_GRENADE] = 0;
+
+    if(m_pistol || d->primary==GUN_PISTOL)  // pistol only mode
+    {
+		d->primary = GUN_PISTOL;
+		d->ammo[GUN_PISTOL] = itemstats[GUN_PISTOL].max;
+	}
+	if(m_lss)
+	{
+		d->mag[d->primary] = d->mag[GUN_PISTOL] = 0;
+		d->primary = GUN_KNIFE;
+	}
+	else if(!m_nogun)
+	{
+		d->ammo[GUN_PISTOL] = itemstats[GUN_PISTOL].start-magsize(GUN_PISTOL);
+		d->mag[GUN_PISTOL] = magsize(GUN_PISTOL);
+		d->ammo[d->primary] = itemstats[d->primary].start-magsize(d->primary);
+		d->mag[d->primary] = magsize(d->primary);
+    };
+
+    if (d->hasarmour)
+    {
+        if(gamemode==m_arena) d->armour = 100;
+	};
+
+	d->gunselect = d->primary;
+};
+
+void item(int num)
+{
+      if (num>0 && num<7) player1->nextprimary = num;
+
+      //if (num==6) conoutf("sorry, no nades yet");
+
+      if (num==7) { player1->hasarmour=!player1->hasarmour;  /*toggle armour*/ };
+};
+
+COMMAND(item,ARG_1INT);
+
+// Added by Rick
+bool intersect(entity *e, vec &from, vec &to, vec *end) // if lineseg hits entity bounding box(entity version)
+{
+    mapmodelinfo &mmi = getmminfo(e->attr2);
+    if(!&mmi || !mmi.h) return false;
+    
+    float lo = (float)(S(e->x, e->y)->floor+mmi.zoff+e->attr3);
+    float hi = lo+mmi.h;
+    vec v = to, w = { e->x, e->y, lo + (fabs(hi-lo)/2.0f) }, *p; 
+    vsub(v, from);
+    vsub(w, from);
+    float c1 = dotprod(w, v);
+
+    if(c1<=0) p = &from;
+    else
+    {
+        float c2 = dotprod(v, v);
+        if(c2<=c1) p = &to;
+        else
+        {
+            float f = c1/c2;
+            vmul(v, f);
+            vadd(v, from);
+            p = &v;
+        };
+    };
+                        
+    if (p->x <= e->x+mmi.rad
+        && p->x >= e->x-mmi.rad
+        && p->y <= e->y+mmi.rad
+        && p->y >= e->y-mmi.rad
+        && p->z <= hi
+        && p->z >= lo)
+     {
+          if (end) *end = *p;
+          return true;
+     }
+     return false;
+};
+// End add by Ricks
---
+++ source/src/protos.h
@@ -0,0 +1,296 @@
+// protos for ALL external functions in cube... 
+
+// command
+extern int variable(char *name, int min, int cur, int max, int *storage, void (*fun)(), bool persist);
+extern void setvar(char *name, int i);
+extern int getvar(char *name);
+extern bool identexists(char *name);
+extern bool addcommand(char *name, void (*fun)(), int narg);
+extern int execute(char *p, bool down = true);
+extern void exec(char *cfgfile);
+extern bool execfile(char *cfgfile);
+extern void resetcomplete();
+extern void complete(char *s);
+extern void alias(char *name, char *action);
+extern char *getalias(char *name);
+extern void writecfg();
+
+// console
+extern void keypress(int code, bool isdown, int cooked);
+extern void renderconsole();
+// extern void conoutf(const char *s, int a = 0, int b = 0, int c = 0); Moved to cube.h by Rick
+extern char *getcurcommand();
+
+// menus
+extern bool rendermenu();
+extern void menuset(int menu);
+extern void menumanual(int m, int n, char *text, char *action=NULL);
+extern void sortmenu(int m, int start, int num);
+extern bool menukey(int code, bool isdown);
+extern void newmenu(char *name);
+extern void rendermenumdl();
+extern void purgemenu(int m);
+
+// serverbrowser
+extern void addserver(char *servername);
+extern char *getservername(int n);
+extern bool resolverwait(const char *name, ENetAddress *address);
+extern void writeservercfg();
+
+// rendergl
+extern void gl_init(int w, int h);
+extern void cleangl();
+extern void gl_drawframe(int w, int h, float changelod, float curfps);
+extern bool installtex(int tnum, char *texname, int &xs, int &ys, bool clamp = false, bool highqual=false);
+extern void mipstats(int a, int b, int c);
+extern void vertf(float v1, float v2, float v3, sqr *ls, float t1, float t2);
+extern void addstrip(int tex, int start, int n);
+extern int lookuptexture(int tex, int &xs, int &ys);
+extern char *hudgunnames[];
+
+// rendercubes
+extern void resetcubes();
+extern void render_flat(int tex, int x, int y, int size, int h, sqr *l1, sqr *l2, sqr *l3, sqr *l4, bool isceil);
+extern void render_flatdelta(int wtex, int x, int y, int size, float h1, float h2, float h3, float h4, sqr *l1, sqr *l2, sqr *l3, sqr *l4, bool isceil);
+extern void render_square(int wtex, float floor1, float floor2, float ceil1, float ceil2, int x1, int y1, int x2, int y2, int size, sqr *l1, sqr *l2, bool topleft);
+extern void render_tris(int x, int y, int size, bool topleft, sqr *h1, sqr *h2, sqr *s, sqr *t, sqr *u, sqr *v);
+extern void addwaterquad(int x, int y, int size);
+extern int renderwater(float hf);
+extern void finishstrips();
+extern void setarraypointers();
+
+// client
+extern void localservertoclient(int chan, uchar *buf, int len);
+extern void connects(char *servername);
+extern void disconnect(int onlyclean = 0, int async = 0);
+extern void toserver(char *text);
+extern void addmsg(int type, const char *fmt = NULL, ...);
+extern bool multiplayer();
+extern bool allowedittoggle();
+extern void sendpackettoserv(int chan, struct _ENetPacket *packet);
+extern void gets2c();
+extern void c2sinfo(dynent *d);
+extern void c2skeepalive();
+extern void neterr(char *s);
+extern void initclientnet();
+extern bool netmapstart();
+extern int getclientnum();
+extern void changemapserv(char *name, int mode);
+extern void newteam(char *name);
+
+// clientgame
+extern flaginfo flaginfos[2];
+extern void mousemove(int dx, int dy); 
+extern void updateworld(int curtime, int lastmillis);
+extern void startmap(char *name);
+extern void changemap(char *name);
+extern void initclient();
+extern void spawnplayer(dynent *d, bool secure=false);
+extern void selfdamage(int damage, int actor, dynent *act, bool gib=false);
+extern dynent *newdynent();
+extern char *getclientmap();
+extern const char *modestr(int n);
+extern void zapdynent(dynent *&d);
+extern dynent *getclient(int cn);
+extern dynent *getbot(int cn); // Added by Rick
+extern void timeupdate(int timeremain);
+extern void resetmovement(dynent *d);
+extern void fixplayer1range();
+extern void respawnself(); // EDIT: AH
+extern void ctf_death();
+//game mode extras
+extern void arenarespawn();
+extern void respawn();
+extern void mastercommand(int cmd, int arg1);
+
+// clientextras
+extern void renderclients();
+extern void renderclient(dynent *d, bool team, char *mdlname, float scale);
+void showscores(bool on);
+extern void renderscores();
+
+// world
+extern void setupworld(int factor);
+extern void empty_world(int factor, bool force);
+extern void remip(block &b, int level = 0);
+extern void remipmore(block &b, int level = 0);
+extern int closestent();
+extern int findentity(int type, int index = 0);
+extern int findplayerstart(int team, int index);
+extern void trigger(int tag, int type, bool savegame);
+extern void resettagareas();
+extern void settagareas();
+extern entity *newentity(int x, int y, int z, char *what, int v1, int v2, int v3, int v4);
+
+// worldlight
+extern void calclight();
+extern void dodynlight(vec &vold, vec &v, int reach, int strength, dynent *owner);
+extern void cleardlights();
+extern block *blockcopy(block &b);
+extern void blockpaste(block &b);
+extern void freeblock(block *&b);
+
+// worldrender
+extern void render_world(float vx, float vy, float vh, float changelod, int yaw, int pitch, float widef, int w, int h);
+extern int lod_factor();
+
+// worldocull
+extern void computeraytable(float vx, float vy);
+extern int isoccluded(float vx, float vy, float cx, float cy, float csize);
+
+// main
+extern void fatal(char *s, char *o = "");
+extern void keyrepeat(bool on);
+extern void show_out_of_renderloop_progress(float bar1, const char *text1, float bar2 = 0, const char *text2 = NULL);
+
+// rendertext
+extern void draw_text(const char *str, int left, int top);
+extern void draw_textf(const char *fstr, int left, int top, ...);
+extern int text_width(const char *str, int limit = -1);
+extern int text_visible(const char *str, int max);
+extern void draw_envbox(int t, int fogdist);
+
+// editing
+extern void cursorupdate();
+extern void toggleedit();
+extern void editdrag(bool isdown);
+extern void setvdeltaxy(int delta, block &sel);
+extern void editequalisexy(bool isfloor, block &sel);
+extern void edittypexy(int type, block &sel);
+extern void edittexxy(int type, int t, block &sel);
+extern void editheightxy(bool isfloor, int amount, block &sel);
+extern bool noteditmode();
+extern void pruneundos(int maxremain = 0);
+
+// renderextras
+extern void line(int x1, int y1, float z1, int x2, int y2, float z2);
+extern void box(block &b, float z1, float z2, float z3, float z4);
+extern void dot(int x, int y, float z);
+extern void linestyle(float width, int r, int g, int b);
+extern void newsphere(vec &o, float max, int type);
+extern void renderspheres(int time);
+extern void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwater);
+extern void readdepth(int w, int h);
+extern void blendbox(int x1, int y1, int x2, int y2, bool border, int tex = -1);
+extern void damageblend(int n);
+extern void addshotline(dynent *d, vec &from, vec &to);
+extern void rendershotlines();
+extern void shotlinereset();
+extern void renderphysents();
+extern bool scoped;
+extern void addgib(dynent *d);
+extern void loadingscreen();
+
+// renderparticles
+extern void setorient(vec &r, vec &u);
+extern void particle_splash(int type, int num, int fade, vec &p);
+//extern void particle_splash(int num, int fade, vec &p);
+// extern void particle_trail(int type, int fade, vec &from, vec &to); Moved to cube.h by Rick
+extern void render_particles(int time);
+
+// worldio
+extern void save_world(char *fname);
+extern void load_world(char *mname);
+extern void writemap(char *mname, int msize, uchar *mdata);
+extern uchar *readmap(char *mname, int *msize);
+extern void loadgamerest();
+extern void incomingdemodata(int chan, uchar *buf, int len, bool extras = false);
+extern void demoplaybackstep();
+extern void stop();
+extern void stopifrecording();
+extern void demodamage(int damage, vec &o);
+extern void demoblend(int damage);
+
+// physics
+extern void moveplayer(dynent *pl, int moveres, bool local);
+extern bool collide(dynent *d, bool spawn, float drop, float rise);
+extern void entinmap(dynent *d);
+extern void setentphysics(int mml, int mmr);
+extern void physicsframe();
+extern void movephysent(physent *pl);
+extern physent *new_physent();
+extern void mphysents();
+
+// sound
+extern void playsound(int n, vec *loc = 0);
+extern void playsoundc(int n);
+extern void initsound();
+extern void cleansound();
+extern void botplaysound(int n, dynent *b); // Added by Rick
+extern void music(char *name);
+
+// rendermd2
+extern void rendermodel(char *mdl, int frame, int range, int tex, float rad, float x, float y, float z, float yaw, float pitch, bool teammate, float scale, float speed, int snap = 0, int basetime = 0, bool oculling=true, dynent *d=NULL);
+extern mapmodelinfo &getmminfo(int i);
+
+// rendermd3
+extern void rendermd3gun();
+extern void loadweapons();
+
+// server
+enum { DISC_NONE = 0, DISC_EOP, DISC_CN, DISC_MKICK, DISC_MBAN, DISC_TAGT, DISC_BANREFUSE, DISC_WRONGPW, DISC_MLOGINFAIL, DISC_MAXCLIENTS };
+
+extern void initserver(bool dedicated, int uprate, char *sdesc, char *ip, char *master, char *passwd, int maxcl, char *maprot, char *masterpwd); // EDIT: AH
+extern void cleanupserver();
+extern void localconnect();
+extern void localdisconnect();
+extern void localclienttoserver(int chan, struct _ENetPacket *);
+extern void serverslice(int seconds, unsigned int timeout);
+extern void putint(ucharbuf &p, int n);
+extern int getint(ucharbuf &p);
+extern void putuint(ucharbuf &p, int n);
+extern int getuint(ucharbuf &p);
+extern void sendstring(const char *t, ucharbuf &p);
+extern void getstring(char *t, ucharbuf &p, int len = MAXTRANS);
+extern void startintermission();
+extern void restoreserverstate(vector<entity> &ents);
+extern uchar *retrieveservers(uchar *buf, int buflen);
+extern char msgsizelookup(int msg);
+extern void serverms(int mode, int numplayers, int minremain, char *smapname, int seconds, bool isfull);
+extern void servermsinit(const char *master, char *sdesc, bool listen);
+extern void sendmaps(int n, string mapname, int mapsize, uchar *mapdata);
+extern ENetPacket *recvmap(int n);
+
+// weapon
+//extern void selectgun(int a = -1, int b = -1, int c =-1);
+extern void shoot(dynent *d, vec &to);
+extern void shootv(int gun, vec &from, vec &to, dynent *d = 0, bool local = false, int nademillis=0);
+extern void createrays(vec &from, vec &to);
+extern void moveprojectiles(float time);
+extern void projreset();
+extern dynent *playerincrosshair();
+extern int reloadtime(int gun);
+extern void reload(dynent *d);
+extern int attackdelay(int gun);
+extern int magsize(int gun);
+extern int kick_rot(int gun);
+extern int kick_back(int gun);
+extern bool gun_changed;
+extern bool akimboside;
+extern void checkweaponswitch();
+extern void weaponswitch(int gun);
+extern void setscope(bool activate);
+// Added by Rick
+extern dynent *botincrosshair();
+extern bool intersect(dynent *d, vec &from, vec &to, vec *end = NULL);
+// End add by Rick
+
+// entities
+extern void renderents();
+extern void putitems(ucharbuf &p);
+extern void realpickup(int n, dynent *d);
+extern void flagaction(int team, int action);
+extern void renderentities();
+extern void resetspawns();
+extern void setspawn(int i, bool on);
+extern void baseammo(int gun);
+extern void checkitems();
+extern void radd(dynent *d);
+// Added by Rick
+extern void botbaseammo(int gun, dynent *d);
+extern bool intersect(entity *e, vec &from, vec &to, vec *end=NULL);
+// End add by Rick
+
+// rndmap
+extern void perlinarea(block &b, int scale, int seed, int psize);
+
