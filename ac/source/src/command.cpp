608d40317141a521889debfcf980219150df0b37
Lee Salzman <lsalzman@gmail.com>
2006-11-13 15:46:24 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@638 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/command.cpp
+++ ac/source/src/command.cpp
@@ -335,7 +335,7 @@ int rndn(int a)    { return a>0 ? rnd(a) : 0; };  COMMANDN(rnd, rndn, ARG_1EXP);
 
 void writecfg()
 {
-    s_sprintfd(cfgname)("config%ssaved.cfg", PATHDIV);
+    s_sprintfd(cfgname)("config%csaved.cfg", PATHDIV);
     FILE *f = fopen(cfgname, "w");
     if(!f) return;
     fprintf(f, "// automatically written on exit, DO NOT MODIFY\n// delete this file to have defaults.cfg overwrite these settings\n// modify settings in game, or put settings in autoexec.cfg to override anything\n\n");
