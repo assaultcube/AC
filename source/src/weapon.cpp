2493a4cfdee30f7d53ef05b03cf2bfae9c47589b
driAn <adrian26ch@yahoo.de>
2006-11-25 14:33:00 +0000

    invalid free fixed
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@725 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/weapon.cpp
+++ source/src/weapon.cpp
@@ -9,25 +9,12 @@ const int SGRAYS = 21;  //down from 21, must be 32 or less (default)
 const float SGSPREAD = 2;
 vec sg[SGRAYS];
 
-//change the particales
-/*guninfo guns[NUMGUNS] =
-{    
-    { S_KNIFE,    S_NULL,     0,      500,    50,     0,   0,  1,    1,   1,    0,  0,	false,	"knife"   },
-    { S_PISTOL,   S_RPISTOL,  1400,   170,    20,     0,   0, 80,   10,   8,    6,  5,  false,	"pistol"  },  // *SGRAYS
-    { S_SHOTGUN,  S_RSHOTGUN, 2400,   1000,   6,      0,   0,  1,   35,   7,    9,  9,  false,	"shotgun" },  //reload time is for 1 shell from 7 too powerful to 6
-    { S_SUBGUN,   S_RSUBGUN,  1650,   80,     17,     0,   0, 70,   15,   30,   1,  2,  true,	"subgun"  },
-    { S_SNIPER,   S_RSNIPER,  1950,   1500,   72,     0,   0, 60,   50,   5,    4,  4,  false,	"sniper"  },
-    { S_ASSAULT,  S_RASSAULT,  2000,   130,   25,     0,   0, 20,   40,   20,   0,  2,  true,	"assault"  },  //recoil was 44
-    { S_NULL,	S_NULL,     1000,   1200,   150,    20,  6,  1,    1,   1,    3,  1,  "grenade" },
-};*/
-
-// fixme: tmp gun test
 guninfo guns[NUMGUNS] =
 {    
     { S_KNIFE,		S_NULL,     0,      500,    50,     0,   0,  1,    1,   1,    0,  0,    false,	"knife"   },
     { S_PISTOL,		S_RPISTOL,  1400,   170,    19,     0,   0, 80,   10,   8,    6,  5,  false,	"pistol"  },  // *SGRAYS
     { S_SHOTGUN,	S_RSHOTGUN, 2400,   1000,   5,      0,   0,  1,   35,   7,    9,  9,  false,	"shotgun" },  //reload time is for 1 shell from 7 too powerful to 6
-    { S_SUBGUN,		S_RSUBGUN,  1650,   80,     15,     0,   0, 70,   15,   30,   1,  2,  true,	"subgun"  },
+    { S_SUBGUN,		S_RSUBGUN,  1650,   80,		16/*15*/,0,   0, 70,   15,   30,   1,  2,  true,	"subgun"  },
     { S_SNIPER,		S_RSNIPER,  1950,   1500,   85,     0,   0, 60,   50,   5,    4,  4,  false,	"sniper"  },
     { S_ASSAULT,	S_RASSAULT,  2000,   130,   25,     0,   0, 20,   40,   20,   0,  2,  true,	"assault"  },  //recoil was 44
     { S_NULL,		S_NULL,     1000,   1200,   150,    20,  6,  1,    1,   1,    3,  1,  "grenade" },
