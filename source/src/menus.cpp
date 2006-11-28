dcdc613614f74c73ca80a3e1e76056fdb90081f5
Lee Salzman <lsalzman@gmail.com>
2006-11-28 19:25:31 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@748 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/menus.cpp
+++ source/src/menus.cpp
@@ -63,7 +63,7 @@ bool rendermenu()
     s_sprintfd(title)(m.hastitle ? "[ %s menu ]" : "%s", m.name); // EDIT: AH
     int mdisp = m.items.length();
     int w = 0;
-    text_startcolumns();
+    if(!m.hastitle) text_startcolumns();
     loopi(mdisp)
     {
         int x = text_width(m.items[i].text);
@@ -90,7 +90,7 @@ bool rendermenu()
         draw_text(m.items[j].text, x, y);
         y += step;
     };
-    text_endcolumns();
+    if(!m.hastitle) text_endcolumns();
     return true;
 };
 
