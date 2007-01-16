b8b965bdad15dd8d0ce627a71c039fa94f662153
driAn <adrian26ch@yahoo.de>
2007-01-16 21:39:49 +0000

    no message
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@840 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/clients2c.cpp
+++ ac/source/src/clients2c.cpp
@@ -256,7 +256,7 @@ void parsemessages(int cn, playerent *d, ucharbuf &p)
             if(!d) return;
             int actor = getint(p);
 			playerent *act = NULL;
-			s_sprintfd(death)("%s", gib ? "GIBBED" : "fragged");
+			s_sprintfd(death)("%s", gib ? "gibbed" : "fragged");
 
             if(actor==cn)
             {
