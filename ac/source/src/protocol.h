d833791d8e23ee6b9876cc030a14b68f71dffaa8
Lee Salzman <lsalzman@gmail.com>
2007-04-02 23:58:13 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@900 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/protocol.h
+++ ac/source/src/protocol.h
@@ -3,8 +3,8 @@
 #define MAXTRANS 5000                   // max amount of data to swallow in 1 go
 #define CUBE_SERVER_PORT 28763
 #define CUBE_SERVINFO_PORT 28764
-#define PROTOCOL_VERSION 1124            // bump when protocol changes
-#define SAVEGAMEVERSION 1007               // bump if dynent/netprotocol changes or any other savegame/demo data bumped from 5
+#define PROTOCOL_VERSION 1125           // bump when protocol changes
+#define SAVEGAMEVERSION 1007            // bump if dynent/netprotocol changes or any other savegame/demo data bumped from 5
 
 // network messages codes, c2s, c2c, s2c
 enum
