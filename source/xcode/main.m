66d332c43705a4edfa0536efc4aae7f04991149e
core-code <julian@corecode.at>
2007-06-04 19:18:35 +0000

    mac updates
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@1126 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/xcode/main.m
+++ source/xcode/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Play Cube
-//
-//  Created by Julian Mayer on 31.08.05.
-//  Copyright __MyCompanyName__ 2005. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
