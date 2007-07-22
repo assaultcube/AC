2c8568cfd8b6679dacbb5d5c8d354514f1d62251
Lee Salzman <lsalzman@gmail.com>
2007-07-22 00:14:54 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@1172 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/rendercubes.cpp
+++ source/src/rendercubes.cpp
@@ -360,9 +360,9 @@ void setwatercolor(char *r, char *g, char *b, char *a)
     }
     else
     {
-        hdr.watercolor[0] = 60;
-        hdr.watercolor[1] = 60;
-        hdr.watercolor[2] = 48;
+        hdr.watercolor[0] = 20;
+        hdr.watercolor[1] = 25;
+        hdr.watercolor[2] = 20;
         hdr.watercolor[3] = 178;
     }
 }
