0cd1cf21905525f99a09f4bc13aac898195aacd2
driAn <adrian26ch@yahoo.de>
2007-12-25 23:39:22 +0000

    shotline sound fx, grenade explosion fixes
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@1491 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/entity.h
+++ ac/source/src/entity.h
@@ -342,13 +342,11 @@ struct playerent : dynent, playerstate
     weapon *weapons[NUMGUNS];
     weapon *weaponsel, *nextweaponsel, *primweap, *nextprimweap, *lastattackweapon;
 
-    struct bounceent *inhandnade;
-
     poshist history; // Previous stored locations of this player
 
     playerent() : clientnum(-1), plag(0), ping(0), lifesequence(0), frags(0), flagscore(0), lastpain(0), clientrole(CR_DEFAULT),
                   skin(0), nextskin(0), spectating(SM_NONE), followplayercn(0),
-                  weaponsel(NULL), nextweaponsel(NULL), primweap(NULL), nextprimweap(NULL), lastattackweapon(NULL), inhandnade(NULL)
+                  weaponsel(NULL), nextweaponsel(NULL), primweap(NULL), nextprimweap(NULL), lastattackweapon(NULL)
     {
         type = ENT_PLAYER;
         name[0] = team[0] = 0;
