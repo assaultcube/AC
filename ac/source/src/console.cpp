b14539ea45ab731e24f2ee2d7b94843379b91d53
Lee Salzman <lsalzman@gmail.com>
2008-01-12 12:05:00 +0000

    history stuff
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@1573 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/console.cpp
+++ ac/source/src/console.cpp
@@ -225,14 +225,14 @@ int histpos = 0;
 
 void history(int n)
 {
-    if(vhistory.inrange(n))
+    static bool inhistory = true; 
+    if(!inhistory && vhistory.inrange(n))
     {
+        inhistory = true;
         char *buf = vhistory[vhistory.length()-n-1];
-        if(buf[0]=='/')
-        {
-            if(strncmp(buf, "/history ", strlen("/history "))) execute(buf+1); // avoid history-recursion
-        }
+        if(buf[0]=='/') execute(buf+1);
         else toserver(buf);
+        inhistory = false;
     }
 }
 
