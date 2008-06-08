96a021bb85d1e9994434679bceeb8767c7d8973b
Lee Salzman <lsalzman@gmail.com>
2008-06-08 21:40:53 +0000

    removed unnecessary tryspawn
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@2164 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/clientgame.cpp
+++ ac/source/src/clientgame.cpp
@@ -561,7 +561,8 @@ void startmap(const char *name)   // called just after a map load
     particlereset();
     suicided = -1;
     spawncycle = -1;
-    respawnself();
+    if(!m_mp(gamemode)) respawnself();
+    else findplayerstart(player1);
     player1->frags = player1->flagscore = player1->deaths = player1->lifesequence = 0;
     loopv(players) if(players[i]) players[i]->frags = players[i]->flagscore = players[i]->deaths = players[i]->lifesequence = 0;
     s_strcpy(clientmap, name);
