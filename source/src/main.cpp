85361fb5a3f42d88fc264a1cd6aa0d0505adc071
driAn <adrian26ch@yahoo.de>
2008-07-23 22:30:18 +0000

    fixed firstrun sound
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@2204 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -659,6 +659,12 @@ int main(int argc, char **argv)
             if(frames>4) SDL_GL_SwapBuffers();
         }
 
+        if(firstrun && frames>4) // run once
+        {
+            execfile("config/firstrun.cfg");
+            firstrun = false;
+        }
+
 #ifdef _DEBUG
 		if(millis>lastflush+60000) {
 			fflush(stdout); lastflush = millis;
