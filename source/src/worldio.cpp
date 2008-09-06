4ee2dbaddfbf93028e490eec2be0f9be9641f6b7
ac_stef <ac_stef@users.sourceforge.net>
2008-09-06 02:57:11 +0000

    gamemodes 'hunt the flag' & 'keep the flag' added
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@2485 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/worldio.cpp
+++ source/src/worldio.cpp
@@ -299,7 +299,7 @@ bool load_world(char *mname)        // still supports all map formats that have 
             if(e.attr1>32) e.attr1 = 32; // 12_03 and below
         }
 
-        if (hdr.version<MAPVERSION  && strncmp(hdr.head,"CUBE",4)==0)  //only render lights, pl starts and map models on old maps
+        if(hdr.version<MAPVERSION  && strncmp(hdr.head,"CUBE",4)==0)  //only render lights, pl starts and map models on old maps
         {
         		switch(e.type)
         		{
