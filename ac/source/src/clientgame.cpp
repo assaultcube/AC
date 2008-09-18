1e0ff297b2984b56f1c224d939e2fc33988058dd
Lee Salzman <lsalzman@gmail.com>
2008-09-18 08:50:13 +0000

    find default playerstart on map changes
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@2689 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/clientgame.cpp
+++ ac/source/src/clientgame.cpp
@@ -641,7 +641,8 @@ void startmap(const char *name)   // called just after a map load
     particlereset();
     suicided = -1;
     spawncycle = -1;
-    if(m_valid(gamemode)) respawnself();
+    if(m_valid(gamemode) && !m_mp(gamemode)) respawnself();
+    else findplayerstart(player1);
     player1->frags = player1->flagscore = player1->deaths = player1->lifesequence = 0;
     loopv(players) if(players[i]) players[i]->frags = players[i]->flagscore = players[i]->deaths = players[i]->lifesequence = 0;
     s_strcpy(clientmap, name);
