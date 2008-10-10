257b8c7e7452ebe6a439af165dda4be01f6f94c3
driAn <adrian26ch@yahoo.de>
2008-10-10 19:40:13 +0000

    tweaked akimob/subgun recoil
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@2877 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/entity.h
+++ source/src/entity.h
@@ -64,11 +64,11 @@ static guninfo guns[NUMGUNS] =
     { "knife",      S_KNIFE,      S_NULL,     0,      500,    50,     0,   0,  1,    1,   1,    0,  0,    0,  0,      0,      0,    1,      false },
     { "pistol",     S_PISTOL,     S_RPISTOL,  1400,   170,    19,     0,   0, 80,   10,   8,    6,  5,    1,  40,     75,     150,  1,      false },
     { "shotgun",    S_SHOTGUN,    S_RSHOTGUN, 2400,   1000,   5,      0,   0,  1,   35,   7,    9,  9,    1,  130,    500,    150,  1,      false },
-    { "subgun",     S_SUBGUN,     S_RSUBGUN,  1650,   80,     16,     0,   0, 70,   15,   30,   1,  2,    3,  20,     60,     200,  1,      true },
+    { "subgun",     S_SUBGUN,     S_RSUBGUN,  1650,   80,     16,     0,   0, 70,   15,   30,   1,  2,    4,  40,     60,     200,  1,      true },
     { "sniper",     S_SNIPER,     S_RSNIPER,  1950,   1500,   85,     0,   0, 60,   50,   5,    4,  4,    1,  100,    500,    100,  1,      false },
     { "assault",    S_ASSAULT,    S_RASSAULT, 2000,   130,    24,     0,   0, 20,   40,   15,   0,  2,    2,  25,     60,     150,  1,      true },
     { "grenade",    S_NULL,       S_NULL,     1000,   650,    200,    20,  6,  1,    1,   1,    3,  1,    0,  0,      0,      0,    3,      false },
-    { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   85,    19,     0,   0, 80,   10,   16,   6,  5,    1,  40,     75,     150,   1,      true },
+    { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   85,     19,     0,   0, 80,   10,   16,   6,  5,    4,  30,     75,     150,   1,      true },
 };
 
 static inline int reloadtime(int gun) { return guns[gun].reloadtime; }
