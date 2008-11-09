a404c1bd8ff3e6ac333cf7e681b6a3c7616a830c
Lee Salzman <lsalzman@gmail.com>
2008-11-09 20:24:06 +0000

    tweaked sniper to 81 damage
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@3475 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/entity.h
+++ source/src/entity.h
@@ -69,7 +69,7 @@ static guninfo guns[NUMGUNS] =
     { "sniper",     S_SNIPER,     S_RSNIPER,  1950,   1500,   85,     0,   0,  1,   50,   5,    4,  4,    1,  100,    500,    100,  1,      false },
     { "assault",    S_ASSAULT,    S_RASSAULT, 2000,   130,    24,     0,   0, 20,   40,   15,   0,  2,    2,  25,     60,     150,  1,      true },
     { "grenade",    S_NULL,       S_NULL,     1000,   650,    200,    20,  6,  1,    1,   1,    3,  1,    0,  0,      0,      0,    3,      false },
-    { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   85,     19,     0,   0, 80,   10,   16,   6,  5,    4,  30,     75,     150,   1,      true },
+    { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   81,     19,     0,   0, 80,   10,   16,   6,  5,    4,  30,     75,     150,   1,      true },
 };
 
 static inline int reloadtime(int gun) { return guns[gun].reloadtime; }
