8a171349938d14a6193860f3c44c8ba47c999914
driAn <adrian26ch@yahoo.de>
2008-11-16 21:47:01 +0000

    protocol, master uri, var fixes
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk@3572 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/cube.h
+++ ac/source/src/cube.h
@@ -52,7 +52,7 @@ extern int verbose;
 #include "protos.h"				// external function decls
 
 #define AC_VERSION 1000
-#define AC_MASTER_URI "masterserver094.cubers.net/cgi-bin/actioncube.pl/" // FIXME, change DNS on ac release
+#define AC_MASTER_URI "masterserver.cubers.net/cgi-bin/actioncube.pl/" // FIXME, change DNS on ac release
 
 #endif
 
