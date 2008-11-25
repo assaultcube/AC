90d2f390404a1ed7db06c30aafc90c4e9fed1f31
Lee Salzman <lsalzman@gmail.com>
2008-11-25 05:18:09 +0000

    different s3 bug workaround
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/branches/v01_00_xx@3754 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/vertmodel.h
+++ ac/source/src/vertmodel.h
@@ -519,15 +519,15 @@ struct vertmodel : model
                     return;
                 }
 
-				extern int s3_dlist_bug;
-                if(isstat && !owner->model->vertexlight && !s3_dlist_bug) glNewList(statlist = glGenLists(1), GL_COMPILE);
+                bool builddlist = isstat && !owner->model->vertexlight;
+                if(builddlist) glNewList(statlist = glGenLists(1), GL_COMPILE);
                 loopi(numdyndraws)
                 {
                     const drawcall &d = dyndraws[i];
-                    if(hasDRE) glDrawRangeElements_(d.type, d.minvert, d.maxvert, d.count, GL_UNSIGNED_SHORT, &dynidx[d.start]);
+                    if(hasDRE && !builddlist) glDrawRangeElements_(d.type, d.minvert, d.maxvert, d.count, GL_UNSIGNED_SHORT, &dynidx[d.start]);
                     else glDrawElements(d.type, d.count, GL_UNSIGNED_SHORT, &dynidx[d.start]);
                 }
-                if(isstat && !owner->model->vertexlight && !s3_dlist_bug)
+                if(builddlist)
                 {
                     glEndList();
                     glCallList(statlist);
