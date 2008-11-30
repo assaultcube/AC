062914343e64e7a70f686cb2d0b09bed2309e348
ac_stef <ac_stef@users.sourceforge.net>
2008-11-30 20:34:21 +0000

    tktf score message extended
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/branches/v01_00_xx@3806 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- ac/source/src/clientgame.cpp
+++ ac/source/src/clientgame.cpp
@@ -811,7 +811,7 @@ void flagmsg(int flag, int message, int actor, int flagtime)
             playsound(S_VOTEPASS, SP_HIGHEST); // need better ktf sound here
             const char *ta = firstperson ? "you have" : colorname(act);
             const char *tb = firstperson ? "" : " has";
-            const char *tc = teammate && !firstperson ? "your teammate " : "";
+            const char *tc = firstperson ? "" : flagteam;
             int m = flagtime / 60;
             if(m)
                 hudoutf("\f2%s%s%s kept the flag for %d minute%s %d seconds now", tc, ta, tb, m, m == 1 ? "" : "s", flagtime % 60);
