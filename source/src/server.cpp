d74b027b95dc9a82de0b3982f1a6681c8e6481e9
Lee Salzman <lsalzman@gmail.com>
2008-12-08 07:47:58 +0000

    packet freeing cleanup
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@3939 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -3078,7 +3078,6 @@ void process(ENetPacket *packet, int sender, int chan)   // sender may be -1
 void localclienttoserver(int chan, ENetPacket *packet)
 {
     process(packet, 0, chan);
-    if(!packet->referenceCount) enet_packet_destroy(packet);
 }
 
 client &addclient()
