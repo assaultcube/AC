52dfc71551afe665773bdf278f23b96f21071f34
ac_stef <ac_stef@users.sourceforge.net>
2009-05-07 10:09:52 +0000

    logging fixes
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4384 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -3943,10 +3943,10 @@ void initserver(bool dedicated)
 
     string identity;
     if(scl.logident[0]) s_sprintf(identity)("[%s]", scl.logident);
-    else s_sprintf(identity)("%s[%d]", scl.ip[0] ? scl.ip : "local", scl.serverport);
+    else s_sprintf(identity)("[%s|%d]", scl.ip[0] ? scl.ip : "local", scl.serverport);
     logger = newlogger(identity, scl.syslogfacility);
     if(dedicated) logger->open(); // log on ded servers only
-    logger->writeline(log::info, "logging local AssaultCube server (version %d, protocol %d/%d) now..", AC_VERSION, PROTOCOL_VERSION, EXT_VERSION);
+    logger->writeline(log::info, "logging local AssaultCube server (version %d, protocol %d/%d, logident %s(%d)) now..", AC_VERSION, PROTOCOL_VERSION, EXT_VERSION, identity, scl.syslogfacility);
 
     if((isdedicated = dedicated))
     {
@@ -3968,6 +3968,9 @@ void initserver(bool dedicated)
             if(scl.mapperm[0]) logger->writeline(log::info,"map permission string: \"%s\"", scl.mapperm);
             logger->writeline(log::info,"server description: \"%s\"", scl.servdesc_full);
             if(scl.servdesc_pre[0] || scl.servdesc_suf[0]) logger->writeline(log::info,"custom server description: \"%sCUSTOMPART%s\"", scl.servdesc_pre, scl.servdesc_suf);
+            logger->writeline(log::info,"maxclients: %d, kick threshold: %d, ban threshold: %d", scl.maxclients, scl.kickthreshold, scl.banthreshold);
+            if(scl.master) logger->writeline(log::info,"master server URL: \"%s\"", scl.master);
+            if(scl.serverpassword[0]) logger->writeline(log::info,"server password: \"%s\"", scl.serverpassword);
         }
     }
 
@@ -3978,7 +3981,8 @@ void initserver(bool dedicated)
         #ifdef WIN32
         SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
         #endif
-        logger->writeline(log::info, "dedicated server started, waiting for clients...\nCtrl-C to exit\n");
+        logger->writeline(log::info, "dedicated server started, waiting for clients...");
+        logger->writeline(log::info, "Ctrl-C to exit");
         atexit(enet_deinitialize);
         atexit(cleanupserver);
         enet_time_set(0);
