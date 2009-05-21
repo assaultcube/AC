fd51f08210f89016903b1d8b17c7e7c54196aaed
ac_stef <ac_stef@users.sourceforge.net>
2009-05-21 21:53:49 +0000

    remove openal-dependency from server (thx nissarin)
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4422 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/pch.h
+++ source/src/pch.h
@@ -1,15 +1,15 @@
 #ifdef __GNUC__
-#ifdef _FORTIFY_SOURCE
-#undef _FORTIFY_SOURCE
-#endif
+    #ifdef _FORTIFY_SOURCE
+        #undef _FORTIFY_SOURCE
+    #endif
 
-#define gamma __gamma
+    #define gamma __gamma
 #endif
 
 #include <math.h>
 
 #ifdef __GNUC__
-#undef gamma
+    #undef gamma
 #endif
 
 #include <string.h>
@@ -20,11 +20,14 @@
 #include <time.h>
 #include <limits.h>
 #ifdef __GNUC__
-#include <new>
-#include <signal.h>
+    #include <new>
+    #include <signal.h>
 #else
-#include <new.h>
+    #include <new.h>
 #endif
+
+#include <zlib.h>
+#include <enet/enet.h>
 
 #ifdef WIN32
     #define WIN32_LEAN_AND_MEAN
@@ -38,29 +41,26 @@
 #endif
 
 #ifndef STANDALONE
-#include <SDL.h>
-#include <SDL_image.h>
+    #include <SDL.h>
+    #include <SDL_image.h>
 
-#define GL_GLEXT_LEGACY
-#define __glext_h__
-#define NO_SDL_GLEXT
-#include <SDL_opengl.h>
-#undef __glext_h__
+    #define GL_GLEXT_LEGACY
+    #define __glext_h__
+    #define NO_SDL_GLEXT
+    #include <SDL_opengl.h>
+    #undef __glext_h__
 
-#include "GL/glext.h"
+    #include "GL/glext.h"
+
+    #ifdef __APPLE__
+        #include "OpenAL/al.h"
+        #include "OpenAL/alc.h"
+        #include "Vorbis/vorbisfile.h"
+    #else
+        #include "AL/al.h"
+        #include "AL/alc.h"
+        #include "vorbis/vorbisfile.h"
+    #endif
+
+    #include <jpeglib.h>
 #endif
-
-#include <zlib.h>
-#include <enet/enet.h>
-
-#ifdef __APPLE__
-#include "OpenAL/al.h" 
-#include "OpenAL/alc.h" 
-#include "Vorbis/vorbisfile.h"
-#else
-#include "AL/al.h" 
-#include "AL/alc.h" 
-#include "vorbis/vorbisfile.h"
-#endif
-
-#include <jpeglib.h>
