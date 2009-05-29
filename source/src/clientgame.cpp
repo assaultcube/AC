32defa394f10463115c2b406238c0e76992d1163
ac_stef <ac_stef@users.sourceforge.net>
2009-05-29 18:17:08 +0000

    fix custom player skins
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4453 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/clientgame.cpp
+++ source/src/clientgame.cpp
@@ -86,6 +86,7 @@ void newname(const char *name)
         c2sinit = false;
         filtertext(player1->name, name, 0, MAXNAMELEN);
         if(!player1->name[0]) s_strcpy(player1->name, "unarmed");
+        updateclientname(player1);
     }
     else conoutf("your name is: %s", player1->name);
     alias("curname", player1->name);
