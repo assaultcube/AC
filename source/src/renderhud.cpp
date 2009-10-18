10116de5aa6be6bf73819871b2ad8d1f9d53b516
flowtron <flowtron@users.sourceforge.net>
2009-10-18 15:36:24 +0000

    colour-coded WQD (cut-offs: 4000, 6000, 8000) and added blendbox for stats
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4662 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -584,7 +584,7 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwat
         blendbox(left - 24, top - 24, VIRTW*2 - 72, VIRTH*2 - 48, true, -1);
         draw_textf("fps %d", left, top, curfps);
         draw_textf("lod %d", left, top+80, lod_factor());
-        draw_textf("wqd \f%s%d", left, top+160, nquads > 3999 ? (nquads > 5999 ? (nquads > 7999 ? "3" : "2") : "1") : "0", nquads); // new: B
+        draw_textf("wqd \f%s%d", left, top+160, nquads > 3999 ? (nquads > 5999 ? (nquads > 7999 ? "3" : "2") : "1") : "0", nquads);
         draw_textf("wvt %d", left, top+240, curvert);
         draw_textf("evt %d", left, top+320, xtraverts);
     }
