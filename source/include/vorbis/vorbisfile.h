f80b7f5bb2d7aa13643cd88503b3f347cefa98df
Lee Salzman <lsalzman@gmail.com>
2009-11-07 09:04:02 +0000

    fixing GCC 4.2 warnings
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4701 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/include/vorbis/vorbisfile.h
+++ source/include/vorbis/vorbisfile.h
@@ -53,28 +53,28 @@ static int _ov_header_fseek_wrap(FILE *f,ogg_int64_t off,int whence){
   return fseek(f,(long)off,whence);
 }
 
-static ov_callbacks OV_CALLBACKS_DEFAULT = {
+static const ov_callbacks OV_CALLBACKS_DEFAULT = {
   (size_t (*)(void *, size_t, size_t, void *))  fread,
   (int (*)(void *, ogg_int64_t, int))           _ov_header_fseek_wrap,
   (int (*)(void *))                             fclose,
   (long (*)(void *))                            ftell
 };
 
-static ov_callbacks OV_CALLBACKS_NOCLOSE = {
+static const ov_callbacks OV_CALLBACKS_NOCLOSE = {
   (size_t (*)(void *, size_t, size_t, void *))  fread,
   (int (*)(void *, ogg_int64_t, int))           _ov_header_fseek_wrap,
   (int (*)(void *))                             NULL,
   (long (*)(void *))                            ftell
 };
 
-static ov_callbacks OV_CALLBACKS_STREAMONLY = {
+static const ov_callbacks OV_CALLBACKS_STREAMONLY = {
   (size_t (*)(void *, size_t, size_t, void *))  fread,
   (int (*)(void *, ogg_int64_t, int))           NULL,
   (int (*)(void *))                             fclose,
   (long (*)(void *))                            NULL
 };
 
-static ov_callbacks OV_CALLBACKS_STREAMONLY_NOCLOSE = {
+static const ov_callbacks OV_CALLBACKS_STREAMONLY_NOCLOSE = {
   (size_t (*)(void *, size_t, size_t, void *))  fread,
   (int (*)(void *, ogg_int64_t, int))           NULL,
   (int (*)(void *))                             NULL,
