2a27edf8a37afd2a542aef560c912777c186223c
Alejandro V. Garcia <buzonyx@gmail.com>
2010-01-14 13:08:12 +0000

    Center window at startup when using the -t switch in MS Windows
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4768 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -384,6 +384,7 @@ void setupscreen(int &usedcolorbits, int &useddepthbits, int &usedfsaa)
     int flags = SDL_RESIZABLE;
     #if defined(WIN32) || defined(__APPLE__)
     flags = 0;
+    putenv("SDL_VIDEO_CENTERED=1"); //Center window
     #endif
     if(fullscreen) flags |= SDL_FULLSCREEN;
     SDL_Rect **modes = SDL_ListModes(NULL, SDL_OPENGL|flags);
