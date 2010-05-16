7c38c551be8d028745f18996bbde6f8a71df70bf
Rafael Barreto <baarreth@hotmail.com>
2010-05-16 23:17:11 +0000

    testing values to sniper and pistol
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4890 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/server.h
+++ source/src/server.h
@@ -394,11 +394,11 @@ itemstat powerupstats[I_ARMOUR-I_HEALTH+1] =
 guninfo guns[NUMGUNS] =
 {
     { "knife",      S_KNIFE,      S_NULL,     0,      500,    50,     0,   0,  1,    1,   1,    0,  0,    0,  0,      0,      0,    1,      false },
-    { "pistol",     S_PISTOL,     S_RPISTOL,  1400,   160,    19,     0,   0, 80,   10,   8,    6,  5,    1,  40,     75,     150,  1,      false },
+    { "pistol",     S_PISTOL,     S_RPISTOL,  1400,   160,    17,     0,   0, 55,   10,   8,    6,  5,    1,  40,     75,     150,  1,      false },
     { "rifle",      S_RIFLE,      S_RRIFLE,   1950,   720,    50,     0,   0, 20,   50,   10,   4,  4,    2,  100,    60,     150,  1,      false }, // temporary values
     { "shotgun",    S_SHOTGUN,    S_RSHOTGUN, 2400,   1000,   5,      0,   0,  1,   35,   7,    9,  9,    1,  130,    500,    150,  1,      false },
     { "subgun",     S_SUBGUN,     S_RSUBGUN,  1650,   80,     14,     0,   0, 70,   15,   30,   1,  2,    5,  15,     55,     250,  1,      true },
-    { "sniper",     S_SNIPER,     S_RSNIPER,  1950,   1500,   85,     0,   0, 60,   50,   5,    4,  4,    1,  100,    500,    100,  1,      false },
+    { "sniper",     S_SNIPER,     S_RSNIPER,  1950,   1500,   80,     0,   0, 60,   50,   5,    4,  4,    1,  100,    500,    100,  1,      false },
     { "assault",    S_ASSAULT,    S_RASSAULT, 2000,   120,    24,     0,   0, 20,   40,   20,   0,  2,    2,  25,     60,     150,  1,      true },
     { "grenade",    S_NULL,       S_NULL,     1000,   650,    200,    20,  6,  1,    1,   1,    3,  1,    0,  0,      0,      0,    3,      false },
     { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   80,     19,     0,   0, 80,   10,   16,   6,  5,    3,  10,     30,     150,  1,      true },
