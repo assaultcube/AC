fed900728ea31a707c61f2f443ef78e76f8b186e
Rafael Barreto <baarreth@hotmail.com>
2010-05-16 00:24:11 +0000

    akimbo recoil changes
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@4878 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/server.h
+++ source/src/server.h
@@ -401,7 +401,7 @@ guninfo guns[NUMGUNS] =
     { "sniper",     S_SNIPER,     S_RSNIPER,  1950,   1500,   85,     0,   0, 60,   50,   5,    4,  4,    1,  100,    500,    100,  1,      false },
     { "assault",    S_ASSAULT,    S_RASSAULT, 2000,   120,    24,     0,   0, 20,   40,   20,   0,  2,    2,  25,     60,     150,  1,      true },
     { "grenade",    S_NULL,       S_NULL,     1000,   650,    200,    20,  6,  1,    1,   1,    3,  1,    0,  0,      0,      0,    3,      false },
-    { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   80,     19,     0,   0, 80,   10,   16,   6,  5,    2,  15,     60,     150,  1,      true },
+    { "pistol",     S_PISTOL,     S_RAKIMBO,  1400,   80,     19,     0,   0, 80,   10,   16,   6,  5,    3,  10,     30,     150,  1,      true },
 };
 
 const char *teamnames[TEAM_NUM+1] = {"CLA", "RVSF", "CLA-SPECT", "RVSF-SPECT", "SPECTATOR", "void"};
