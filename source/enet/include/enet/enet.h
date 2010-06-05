41e12538bc0ebcb4689934dbdd4b2af3f9d9388b
Lee Salzman <lsalzman@gmail.com>
2010-06-05 20:37:19 +0000

    fragment sending fix
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@5144 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/enet/include/enet/enet.h
+++ source/enet/include/enet/enet.h
@@ -522,6 +522,8 @@ extern void                  enet_peer_setup_outgoing_command (ENetPeer *, ENetO
 extern ENetOutgoingCommand * enet_peer_queue_outgoing_command (ENetPeer *, const ENetProtocol *, ENetPacket *, enet_uint32, enet_uint16);
 extern ENetIncomingCommand * enet_peer_queue_incoming_command (ENetPeer *, const ENetProtocol *, ENetPacket *, enet_uint32);
 extern ENetAcknowledgement * enet_peer_queue_acknowledgement (ENetPeer *, const ENetProtocol *, enet_uint16);
+extern void                  enet_peer_dispatch_incoming_unreliable_commands (ENetPeer *, ENetChannel *);
+extern void                  enet_peer_dispatch_incoming_reliable_commands (ENetPeer *, ENetChannel *);
 
 ENET_API void * enet_range_coder_create (void);
 ENET_API void   enet_range_coder_destroy (void *);
