ad67963f88198dc69e461be7ced4ade56f272711
flowtron <flowtron@users.sourceforge.net>
2010-07-28 11:25:17 +0000

    minor tweaks
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@5466 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/weapon.cpp
+++ source/src/weapon.cpp
@@ -107,7 +107,7 @@ void quicknadethrow(bool on)
         if(player1->weapons[GUN_GRENADE]->mag > 0)
         {
             if(player1->weaponsel->type != GUN_GRENADE) selectweapon(player1->weapons[GUN_GRENADE]);
-            if(player1->weaponsel->type == GUN_GRENADE || quicknade_hold ) attack(true);
+            if(player1->weaponsel->type == GUN_GRENADE || quicknade_hold) attack(true); // TODO: use a flag for all weapons to shoot (only action?) once they've successfully reloaded or been switched to. Would need to get purged at appropriate time too.
         }
     }
     else
