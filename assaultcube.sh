f39175a8e00710fcc6a2b13b166fece08e09bcbe
flowtron <flowtron@users.sourceforge.net>
2010-08-01 20:11:28 +0000

    ready for release (TEST BUILD: off, game-home: 1.1)
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@5512 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- assaultcube.sh
+++ assaultcube.sh
@@ -7,7 +7,7 @@ CUBE_DIR=$(dirname $(readlink -f "${0}"))
 
 # CUBE_OPTIONS contains any command line options you would like to start Cube with.
 #CUBE_OPTIONS="-f"
-CUBE_OPTIONS="--home=${HOME}/.assaultcube_v1.1b --init"
+CUBE_OPTIONS="--home=${HOME}/.assaultcube_v1.1 --init"
 
 # SYSTEM_NAME should be set to the name of your operating system.
 #SYSTEM_NAME=Linux
--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -885,13 +885,13 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwat
         draw_textf("SCROLL to change player", left, top+80);
     }
 
-    /* */
+    /* * /
     glLoadIdentity();
 	glOrtho(0, VIRTW*3/2, VIRTH*3/2, 0, -1, 1);
     const int tbMSGleft = (VIRTW*3/2)*5/6;
     const int tbMSGtop = (VIRTH*3/2)*7/8;
     draw_textf("!TEST BUILD!", tbMSGleft, tbMSGtop);
-    /* */
+    / * */
     drawscores();
     if(!hidespecthud && spectating && player1->spectatemode!=SM_DEATHCAM)
     {
