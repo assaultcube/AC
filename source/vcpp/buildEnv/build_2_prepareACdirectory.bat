5398645eb021a2643d071734bbee709c384f3c72
aerkefiende <aerkefiende@bcfh.eu>
2010-10-16 18:32:06 +0000

    Updated Windows packaging
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@5760 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/vcpp/buildEnv/build_2_prepareACdirectory.bat
+++ source/vcpp/buildEnv/build_2_prepareACdirectory.bat
@@ -29,14 +29,16 @@ copy /Y %workingacdir%\packages\models\misc\gib03\shadows.dat %acdir%\packages\m
 copy /Y %workingacdir%\packages\models\pickups\akimbo\shadows.dat %acdir%\packages\models\pickups\akimbo\shadows.dat
 copy /Y %workingacdir%\packages\models\pickups\ammobox\shadows.dat %acdir%\packages\models\pickups\ammobox\shadows.dat
 copy /Y %workingacdir%\packages\models\pickups\health\shadows.dat %acdir%\packages\models\pickups\health\shadows.dat
+copy /Y %workingacdir%\packages\models\pickups\helmet\shadows.dat %acdir%\packages\models\pickups\helmet\shadows.dat
 copy /Y %workingacdir%\packages\models\pickups\kevlar\shadows.dat %acdir%\packages\models\pickups\kevlar\shadows.dat
-copy /Y %workingacdir%\packages\models\pickups\nades\shadows.dat %acdir%\packages\models\pickups\nades\shadows.dat
+copy /Y %workingacdir%\packages\models\pickups\nade\shadows.dat %acdir%\packages\models\pickups\nade\shadows.dat
 copy /Y %workingacdir%\packages\models\pickups\pistolclips\shadows.dat %acdir%\packages\pickups\pistolclips\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\assault\world\shadows.dat %acdir%\packages\models\weapons\assault\world\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\grenade\static\shadows.dat %acdir%\packages\models\weapons\grenade\static\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\grenade\world\shadows.dat %acdir%\packages\models\weapons\grenade\world\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\knife\world\shadows.dat %acdir%\packages\models\weapons\knife\world\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\pistol\world\shadows.dat %acdir%\packages\models\weapons\pistol\world\shadows.dat
+copy /Y %workingacdir%\packages\models\weapons\rifle\world\shadows.dat %acdir%\packages\models\weapons\rifle\world\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\shotgun\world\shadows.dat %acdir%\packages\models\weapons\shotgun\world\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\sniper\world\shadows.dat %acdir%\packages\models\weapons\sniper\world\shadows.dat
 copy /Y %workingacdir%\packages\models\weapons\subgun\world\shadows.dat %acdir%\packages\models\weapons\subgun\world\shadows.dat
