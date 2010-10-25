5fca5d032e6685a5328d687b47b12c86b2d9c3c3
Rafael Barreto <baarreth@hotmail.com>
2010-10-25 15:52:16 +0000

    fixing vote system, checking if cn2boot is valid
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@5788 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/serveractions.h
+++ source/src/serveractions.h
@@ -168,7 +168,7 @@ struct kickaction : playeraction
     {
         wasvalid = false;
         role = roleconf('k');
-        if(isvalid() && strlen(reason) > 3)
+        if(isvalid() && strlen(reason) > 3 && valid_client(cn))
         {
             wasvalid = true;
             formatstring(desc)("kick player %s, reason: %s", clients[cn]->name, reason);
