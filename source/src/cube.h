0937fa632841b32a46c8608d1ef9715b2a7337c7
Rafael Barreto <baarreth@hotmail.com>
2010-11-13 20:41:38 +0000

    linux bins (32 and 64) to 1.1.0.4
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/branches/v01_10_04@5840 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/cube.h
+++ source/src/cube.h
@@ -52,7 +52,7 @@ extern int verbose;
 
 #include "protos.h"				// external function decls
 
-#define AC_VERSION 1103
+#define AC_VERSION 1104
 #define AC_MASTER_URI "assault.cubers.net"
 #define AC_MASTER_PORT 28760
 #define MAXCL 20
