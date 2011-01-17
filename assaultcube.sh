0e996155cfa7a6ff536f1769aafcfe283d6dfb1d
flowtron <flowtron@users.sourceforge.net>
2011-01-17 18:26:24 +0000

    updating docs, changing launch-script
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6011 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- assaultcube.sh
+++ assaultcube.sh
@@ -7,7 +7,8 @@ CUBE_DIR=$(dirname $(readlink -f "${0}"))
 
 # CUBE_OPTIONS contains any command line options you would like to start Cube with.
 #CUBE_OPTIONS="-f"
-CUBE_OPTIONS="--home=${HOME}/.assaultcube_v1.1 --init"
+#CUBE_OPTIONS="--home=${HOME}/.assaultcube_v1.1 --init"
+CUBE_OPTIONS="--home=${HOME}/.assaultcube_svn --init"
 
 # SYSTEM_NAME should be set to the name of your operating system.
 #SYSTEM_NAME=Linux
