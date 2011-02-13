fc66447e184764e0f56bb699c256ef649220fc46
Lee Salzman <lsalzman@gmail.com>
2011-02-13 18:02:13 +0000

    make ac_client.exe a native windows app rather than console
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6097 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- assaultcube_release.bat
+++ assaultcube_release.bat
@@ -1,2 +1,1 @@
-bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\AssaultCube_v1.1" --init %1 %2 %3 %4 %5
-pause
+start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\AssaultCube_v1.1" --init %1 %2 %3 %4 %5
