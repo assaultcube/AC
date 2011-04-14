67392bd0fc63878468165c113d51963299904573
Lee Salzman <lsalzman@gmail.com>
2011-04-14 14:36:05 +0000

    cleaner Mersenne Twister implementation
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6247 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/crypto.cpp
+++ source/src/crypto.cpp
@@ -830,48 +830,35 @@ bool checkchallenge(const char *answerstr, void *correct)
 
 ////////////////////////// crypto rand ////////////////////////////////////////
 
-#define N              (624)             
-#define M              (397)                
-#define K              (0x9908B0DFU)       
-#define hiBit(u)       ((u) & 0x80000000U)  
-#define loBit(u)       ((u) & 0x00000001U)  
-#define loBits(u)      ((u) & 0x7FFFFFFFU)  
-#define mixBits(u, v)  (hiBit(u)|loBits(v)) 
+#define N (624)             
+#define M (397)                
+#define K (0x9908B0DFU)       
 
-static uint state[N+1];
-static uint *next;
-static int left = -1;
+static uint state[N];
+static int next = N;
 
 void seedMT(uint seed)
 {
-    register uint x = (seed | 1U) & 0xFFFFFFFFU, *s = state;
-    register int j;
-    for(left=0, *s++=x, j=N; --j; *s++ = (x*=69069U) & 0xFFFFFFFFU);
+    state[0] = seed;
+    for(uint i = 1; i < N; i++)
+        state[i] = seed = 1812433253U * (seed ^ (seed >> 30)) + i;
+    next = 0;
 }
 
-uint reloadMT(void)
+uint randomMT()
 {
-    register uint *p0=state, *p2=state+2, *pM=state+M, s0, s1;
-    register int j;
-    if(left < -1) seedMT(time(NULL));
-    left=N-1, next=state+1;
-    for(s0=state[0], s1=state[1], j=N-M+1; --j; s0=s1, s1=*p2++) *p0++ = *pM++ ^ (mixBits(s0, s1) >> 1) ^ (loBit(s1) ? K : 0U);
-    for(pM=state, j=M; --j; s0=s1, s1=*p2++) *p0++ = *pM++ ^ (mixBits(s0, s1) >> 1) ^ (loBit(s1) ? K : 0U);
-    s1=state[0], *p0 = *pM ^ (mixBits(s0, s1) >> 1) ^ (loBit(s1) ? K : 0U);
-    s1 ^= (s1 >> 11);
-    s1 ^= (s1 <<  7) & 0x9D2C5680U;
-    s1 ^= (s1 << 15) & 0xEFC60000U;
-    return(s1 ^ (s1 >> 18));
-}
-
-uint randomMT(void)
-{
-    uint y;
-    if(--left < 0) return(reloadMT());
-    y  = *next++;
+    int cur = next;
+    if(++next >= N)
+    {
+        if(next > N) { seedMT(5489U + time(NULL)); cur = next++; }
+        else next = 0;
+    }
+    uint y = (state[cur] & 0x80000000U) | (state[next] & 0x7FFFFFFFU);
+    state[cur] = y = state[cur < N-M ? cur + M : cur + M-N] ^ (y >> 1) ^ (-(y & 1U) & K);
     y ^= (y >> 11);
     y ^= (y <<  7) & 0x9D2C5680U;
     y ^= (y << 15) & 0xEFC60000U;
-    return(y ^ (y >> 18));
+    y ^= (y >> 18);
+    return y;
 }
 
