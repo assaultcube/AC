9c016add5e882a2f10953d4844d47b2472676864
Bukz <bukizzle316@gmail.com>
2011-04-14 11:20:51 +0000

    this conoutf needs "(_" for i18n
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6244 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/weapon.cpp
+++ source/src/weapon.cpp
@@ -696,7 +696,7 @@ void accuracyreset()
     {
         accuracym[i].hits=accuracym[i].shots=0;
     }
-    conoutf("Your accuracy has been reset.");
+    conoutf(_("Your accuracy has been reset."));
 }
 COMMAND(accuracyreset,ARG_NONE);
 // weapon
