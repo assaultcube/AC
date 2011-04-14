6fadd3beaf1fcf595471e741d338dc5498fec2df
sireus <sireus@users.souceforge.net>
2011-04-14 14:11:25 +0000

    bots now actually fire the rifle
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6246 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/bot/ac_bot_ai.cpp
+++ source/src/bot/ac_bot_ai.cpp
@@ -21,7 +21,7 @@ weaponinfo_s WeaponInfoTable[MAX_WEAPONS] =
     // ---- Type : -- minDD -- maxDD -- minFD -- maxFD -- mA
      { TYPE_MELEE,      0.0f,    2.0f,    0.0f,    3.5f,   1 }, // KNIFE
      { TYPE_NORMAL,     0.0f,   20.0f,    0.0f,   50.0f,   6 }, // PISTOL
-     { TYPE_SNIPER,    20.0f,   80.0f,    0.0f,    0.0f,   2 }, // RIFLE
+     { TYPE_SNIPER,    20.0f,   80.0f,    5.0f,  180.0f,   2 }, // RIFLE     // min and max FD just guesstimates for now
      { TYPE_SHOTGUN,    0.0f,   15.0f,    0.0f,   40.0f,   3 }, // SHOTGUN
      { TYPE_AUTO,       0.0f,   25.0f,    0.0f,   60.0f,  10 }, // SUBGUN
      { TYPE_SNIPER,    23.0f,   75.0f,    20.0f, 200.0f,   3 }, // SNIPER
