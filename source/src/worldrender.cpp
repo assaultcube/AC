a24fabce9a42b61a24dfcb5f769bc8e9bfeda4e3
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2011-05-14 12:12:28 +0000

    map overview spectate mode ?
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6425 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/worldrender.cpp
+++ source/src/worldrender.cpp
@@ -270,7 +270,7 @@ void distlod(int &low, int &high, int angle, float widef)
 void render_world(float vx, float vy, float vh, float changelod, int yaw, int pitch, float fov, float fovy, int w, int h)
 {
     loopi(LARGEST_FACTOR) stats[i] = 0;
-    min_lod = minimap ? MAX_LOD : MIN_LOD+abs(pitch)/12;
+    min_lod = minimap || (player1->isspectating() && player1->spectatemode == SM_OVERVIEW) ? MAX_LOD : MIN_LOD+abs(pitch)/12;
     yaw = 360-yaw;
     float widef = fov/75.0f;
     int cdist = abs(yaw%90-45);
