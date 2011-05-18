8bb842309e039234ef0279fa838e1e8d49088188
Bukz <bukizzle316@gmail.com>
2011-05-18 13:29:55 +0000

    wincompat.h license update - thanks Magnus and jamz
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6450 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/include/wincompat.h
+++ source/include/wincompat.h
@@ -2,10 +2,30 @@
 #define WINCOMPAT_INCLUDED
 
 /**
- * 
+ *
  * Author: Magnus Naeslund (mag@fbab.net, mag@bahnhof.se)
  * (c) 2000 Magnus Naeslund, all rights reserved
- * 
+ * Copyright (c) 2000-2011 Magnus Naeslund (magnus.naslund@mucrosolutions.se,
+ * mag@fbab.net, mag@bahnhof.se)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
 #include <sys/time.h>
