7b88ae44aaec9736b6b222346be288941f0ba08a
sireus <sireus@users.souceforge.net>
2011-05-27 14:33:03 +0000

    that's a float
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6466 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/serverfiles.h
+++ source/src/serverfiles.h
@@ -237,7 +237,7 @@ float Mheight = 0;
 
 bool mapisok(mapstats *ms)
 {
-    if ( Mheight > MAXMHEIGHT ) { logline(ACLOG_INFO, "MAP CHECK FAIL: The overall ceil height is too high (%d cubes)", Mheight); return false; }
+    if ( Mheight > MAXMHEIGHT ) { logline(ACLOG_INFO, "MAP CHECK FAIL: The overall ceil height is too high (%.1f cubes)", Mheight); return false; }
     if ( Mopen > MAXMAREA ) { logline(ACLOG_INFO, "MAP CHECK FAIL: There is a big open area in this (hint: use more solid walls)", Mheight); return false; }
 
     if ( ms->hasflags ) // Check if flags are ok
