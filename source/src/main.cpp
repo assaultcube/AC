f6290ca5b86d2f97e18ac2dd8bb7d42b751f4a26
Bukz <bukizzle316@gmail.com>
2012-06-16 22:17:10 +0000

    ac_avenue
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6838 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -883,12 +883,12 @@ const char *rndmapname()
 {
     // "ac_depot_classic",
     static const char * const mapnames[] = {
-        "ac_aqueous",  "ac_arabian",  "ac_arctic",  "ac_arid", "ac_complex",
-        "ac_depot",  "ac_desert",  "ac_desert2", "ac_desert3",  "ac_douze",
-        "ac_edifice",  "ac_elevation",  "ac_gothic",  "ac_iceroad",  "ac_industrial",
-        "ac_ingress",  "ac_keller",  "ac_mines",  "ac_outpost",  "ac_power",
-        "ac_rattrap",  "ac_scaffold",  "ac_shine",  "ac_snow",  "ac_stellar",
-        "ac_sunset",  "ac_toxic",  "ac_urban",  "ac_werk",
+        "ac_aqueous",  "ac_arabian",  "ac_arctic",  "ac_arid", "ac_avenue",
+        "ac_complex", "ac_depot",  "ac_desert",  "ac_desert2", "ac_desert3",
+        "ac_douze", "ac_edifice",  "ac_elevation",  "ac_gothic",  "ac_iceroad",
+        "ac_industrial", "ac_ingress",  "ac_keller",  "ac_mines",  "ac_outpost",
+        "ac_power", "ac_rattrap",  "ac_scaffold",  "ac_shine",  "ac_snow",
+        "ac_stellar", "ac_sunset",  "ac_toxic",  "ac_urban",  "ac_werk",
     };
     int l = sizeof(mapnames)/sizeof(mapnames[0]);
     int n = rnd(l);
