0228bbd9e5b8486d9b7e4729c722a7ea6e0ce32f
RandumKiwi <RandumKiwi@users.noreply.github.com>
2012-09-24 04:32:48 +0000

    Deletion of reference.xml material.
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6888 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- docs/autogen/win32_cubedoc2xhtml.ps1
+++
@@ -1,8 +0,0 @@
-$files = dir ..\* -include *.html
-
-foreach($f in $files)
-{
-    $in = $f.FullName
-    $out = [system.io.path]::ChangeExtension($f.FullName, "html")
-    msxsl.exe -o "$out" -v "$in" ..\transformations\cubedoc2xhtml.xslt
-}
--- docs/autogen/win32_cuberef2xhtml.bat
+++
@@ -1,2 +0,0 @@
-msxsl.exe -o ..\out.html -v ..\reference.html ..\transformations\cuberef2xhtml.xslt
-pause
