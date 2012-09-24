f5b3f1cd0faa04121f89570ef863dd73cc6ed009
RandumKiwi <RandumKiwi@users.noreply.github.com>
2012-09-24 04:32:48 +0000

    Deletion of reference.xml material.
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6888 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- docs/autogen/win32_cuberef2cubescript.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem windows only.
-rem automatically transforms the reference to a cubescript.
-rem requires MSXSL, available at
-rem http://www.microsoft.com/downloads/details.aspx?familyid=2fb55371-c94e-4373-b0e9-db4816552e41&displaylang=en
-@echo on
-
-msxsl.exe -o ..\..\config\docs.cfg -v ..\reference.html ..\transformations\cuberef2cubescript.xslt
-pause
