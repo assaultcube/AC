e2c7bd9e4fcdc16589ad77be56b4d3effb9b6425
aerkefiende <aerkefiende@bcfh.eu>
2012-12-04 18:07:53 +0000

    Set new version value for the windows batch file
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6943 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- assaultcube_release.bat
+++ assaultcube_release.bat
@@ -1,1 +1,1 @@
-start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\AssaultCube_v1.1" --init %1 %2 %3 %4 %5
+start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\AssaultCube_v1.2" --init %1 %2 %3 %4 %5
