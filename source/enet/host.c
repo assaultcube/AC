c09d8952ea32c9727605673bf392071b1d366d49
Lee Salzman <lsalzman@gmail.com>
2012-12-13 14:17:44 +0000

    enet 1.3.6
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@6973 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/enet/host.c
+++ source/enet/host.c
@@ -3,6 +3,7 @@
  @brief ENet host management functions
 */
 #define ENET_BUILDING_LIB 1
+#define __MINGW_USE_VC2005_COMPAT 1
 #include <string.h>
 #include <time.h>
 #include "enet/enet.h"
@@ -102,6 +103,8 @@ enet_host_create (const ENetAddress * address, size_t peerCount, size_t channelL
     host -> compressor.decompress = NULL;
     host -> compressor.destroy = NULL;
 
+    host -> intercept = NULL;
+
     enet_list_clear (& host -> dispatchQueue);
 
     for (currentPeer = host -> peers;
@@ -133,6 +136,9 @@ void
 enet_host_destroy (ENetHost * host)
 {
     ENetPeer * currentPeer;
+
+    if (host == NULL)
+      return;
 
     enet_socket_destroy (host -> socket);
 
