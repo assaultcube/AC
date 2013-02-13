300da0be7e33df79f8cd77bb09aa31aa9dcee00b
Alejandro V. Garcia <buzonyx@gmail.com>
2013-02-13 13:26:12 +0000

    transparency test (old stuff from 2008)
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@7192 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/src/model.h
+++ source/src/model.h
@@ -41,12 +41,13 @@ class dynent;
 
 struct model
 {
-    bool cullface, vertexlight;
+    bool cullface, vertexlight, alphablend;  //ALX Alpha channel models
     float alphatest, translucency, scale, radius, shadowdist;
     vec translate;
     int cachelimit, batch;
-
-    model() : cullface(true), vertexlight(false), alphatest(0.9f), translucency(0.25f), scale(1), radius(0), shadowdist(0), translate(0, 0, 0), cachelimit(8), batch(-1) {}
+    
+    //model() : cullface(true), vertexlight(false), alphatest(0.9f), translucency(0.25f), scale(1), radius(0), shadowdist(0), translate(0, 0, 0), cachelimit(8), batch(-1) {}
+    model() : cullface(true), vertexlight(false),  alphablend(false), alphatest(0.9f), translucency(0.25f), scale(1), radius(0), shadowdist(0), translate(0, 0, 0), cachelimit(8), batch(-1) {}
     virtual ~model() {}
 
     virtual bool load() = 0;
