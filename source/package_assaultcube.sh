441d334bae71e82ff70a046ed98bdd281576780b
RandumKiwi <RandumKiwi@users.noreply.github.com>
2013-03-04 06:41:57 +0000

    Some fixing of Cleaner's b0rkings, being picky about some other things.
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/trunk/ac@7295 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- source/package_assaultcube.sh
+++ source/package_assaultcube.sh
@@ -78,8 +78,8 @@ fi
 
 # Check that map previews are all available:
 MAPSPATH="$PATHTOACDIR/packages/maps/official"
-MAPSCGZ=`cd $MAPSPATH && find ./*.cgz ! -name ac_depot_classic.cgz | xargs -i basename {} .cgz | sort -u | xargs`
-MAPSJPG=`cd $MAPSPATH/preview && find ./*.jpg ! -name ac_depot_classic.jpg | xargs -i basename {} .jpg | sort -u | xargs`
+MAPSCGZ=`cd $MAPSPATH && find ./*.cgz ! -name ac_depot_classic.cgz -name new_content.cgz | xargs -i basename {} .cgz | sort -u | xargs`
+MAPSJPG=`cd $MAPSPATH/preview && find ./*.jpg ! -name ac_depot_classic.jpg -name new_content.cgz | xargs -i basename {} .jpg | sort -u | xargs`
 if [ "$MAPSCGZ" = "$MAPSJPG" ]; then
     echo "All map \"previews\" are available!"
     echo -e "Proceeding to the next step...\n"
@@ -90,7 +90,7 @@ else
 fi
 
 # Check that bot waypoints are all available:
-MAPSCGZ2=`cd $MAPSPATH && find ./*.cgz | xargs -i basename {} .cgz | sort -u | xargs`
+MAPSCGZ2=`cd $MAPSPATH && find ./*.cgz ! -name new_content.cgz | xargs -i basename {} .cgz | sort -u | xargs`
 MAPSBOTS=`cd $PATHTOACDIR/bot/waypoints && find ./*.wpt | xargs -i basename {} .wpt | sort -u | xargs`
 if [ "$MAPSBOTS" = "$MAPSCGZ2" ]; then
     echo "All bot waypoints are available!"
@@ -191,6 +191,8 @@ tar cjvf $SAVETARBALLPATH/AssaultCube_v$NEWACVERSION.tar.bz2 \
   --exclude=*.bat --exclude=*bin_win32* \
   --exclude=*xcode* \
   --exclude=*vcpp* \
+  --exclude=*new.cfg \
+  --exclude=*new_content.cgz \
   --exclude-vcs \
   AssaultCube_v$NEWACVERSION/
 
