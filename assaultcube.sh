8d83d8b8ab8cd098a70237986d3f55131dbbe614
ac-stef <ac_stef@users.sf.net>
2015-02-14 19:48:12 +0100

    change user data path for the dev version
    also: one more directory level

--- assaultcube.sh
+++ assaultcube.sh
@@ -8,7 +8,7 @@ CUBE_DIR=$(dirname "$(readlink -f "${0}")")
 
 # CUBE_OPTIONS starts AssaultCube with any command line options you choose.
 #CUBE_OPTIONS="-f"
-CUBE_OPTIONS="--home=${HOME}/.assaultcube_v1.2 --init"
+CUBE_OPTIONS="--home=${HOME}/.assaultcube/v1.2next --init"
 
 # SYSTEM_NAME should be set to the name of your operating system.
 #SYSTEM_NAME=Linux
