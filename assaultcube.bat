44565a9cb7a460291d1866017aaf302a7457d91c
drian <drian0@protonmail.com>
2021-03-13 05:38:35 +0100

    fixed typo in startup msg

--- assaultcube.bat
+++ assaultcube.bat
@@ -1,1 +1,1 @@
-start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\My Games\AssaultCube\v1.3next" --init %1 %2 %3 %4 %5 -t -w800 -h600
+start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\My Games\AssaultCube\v1.3next" --init %1 %2 %3 %4 %5
