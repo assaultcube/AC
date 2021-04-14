b7f944a3e6bea1e54cb969456896a3168702de51
drian <drian0@protonmail.com>
2021-04-14 06:33:42 +0200

    resolved issue in weapon OOB check

--- source/src/clients2c.cpp
+++ source/src/clients2c.cpp
@@ -792,7 +792,7 @@ void parsemessages(int cn, playerent *d, ucharbuf &p, bool demo = false)
                     armour = getint(p),
                     health = getint(p);
                 playerent *target = getclient(tcn), *actor = getclient(acn);
-                if(!target || !actor || valid_weapon(gun)) break;
+                if(!target || !actor || !valid_weapon(gun)) break;
                 target->armour = armour;
                 target->health = health;
                 dodamage(damage, target, actor, -1, type==SV_GIBDAMAGE, false);
