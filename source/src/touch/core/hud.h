45fc06f49340c161e0b94ac69237ea8786583610
flowtron <flow@flowtron.de>
2021-05-03 14:32:08 +0200

    remove compiler warnings

--- source/src/touch/core/hud.h
+++ source/src/touch/core/hud.h
@@ -38,15 +38,18 @@ struct hud
         }
         else if(x<2/d1)
         {
-            return n1*(x-=1.5/d1)*x+0.75;
+            x -= 1.5/d1;
+            return n1*x*x+0.75;
         }
         else if(x<2.5/d1)
         {
-            return n1*(x-=2.25/d1)*x+0.9375;
+            x -= 2.25/d1;
+            return n1*x*x+0.9375;
         }
         else
         {
-            return n1*(x-=2.625/d1)*x+0.984375;
+            x -= 2.625/d1;
+            return n1*x*x+0.984375;
         }
     }
 
