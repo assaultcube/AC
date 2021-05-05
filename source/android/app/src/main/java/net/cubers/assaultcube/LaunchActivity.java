c0687a238340502685b8388ec84521b35255d15d
drian <drian0@protonmail.com>
2021-05-05 16:29:44 +0200

    #303 app is not responding on very first startup

--- source/android/app/src/main/java/net/cubers/assaultcube/LaunchActivity.java
+++ source/android/app/src/main/java/net/cubers/assaultcube/LaunchActivity.java
@@ -2,6 +2,7 @@ package net.cubers.assaultcube;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,8 @@ public class LaunchActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
+        Activity act = this;
+
         // Unfortunately AssaultCube uses two different API's for file access: SDL_RW from libsdl2 and fopen from cstdio:
         // - libsdl2 targets the app's internal storage meaning the *readonly* APK data itself and this behavior is not easy to change.
         //   AssaultCube reads only few things using SDL_RW such as audio files and certain images. No write access is performed (!)
@@ -30,19 +33,21 @@ public class LaunchActivity extends Activity {
         //   AssaultCube reads most of the things using fopen and it writes *ALL* things using fopen.
         //   Since we cannot easily get rid of the libsdl2 behavior our approach is to extract all data from the APK to the app's external
         //   directory which is writeable and then have all calls to stdio target this directory. We do this once on startup.
-        AssetExporter assetExporter = new AssetExporter();
-        boolean copyAssetsRequired = assetExporter.isAssetExportRequired(this);
-        long showProgressMinimumDurationMilliseconds = copyAssetsRequired ? 1000 : 0;
+        AsyncTask.execute(() -> {
+            AssetExporter assetExporter = new AssetExporter();
+            boolean copyAssetsRequired = assetExporter.isAssetExportRequired(act);
+            long showProgressMinimumDurationMilliseconds = copyAssetsRequired ? 1000 : 0;
 
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            // exports assets if needed
-            if(copyAssetsRequired)
-                assetExporter.copyAssets(this);
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                // exports assets if needed
+                if(copyAssetsRequired)
+                    assetExporter.copyAssets(act);
 
-            // launch game
-            Intent intent = new Intent(this, AssaultCubeActivity.class);
-            startActivity(intent);
-            finish();
-        }, showProgressMinimumDurationMilliseconds);
+                // launch game
+                Intent intent = new Intent(act, AssaultCubeActivity.class);
+                startActivity(intent);
+                finish();
+            }, showProgressMinimumDurationMilliseconds);
+        });
     }
 }
