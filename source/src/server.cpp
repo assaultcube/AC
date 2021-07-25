6c1172d7ef68c8b2805d7c5b0696863bf1cc88c7
elmillion <83298586+elmillion@users.noreply.github.com>
2021-07-25 19:22:46 +0200

    Add shotdamage in player stat buffer

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -4690,7 +4690,8 @@ void extinfo_statsbuf(ucharbuf &p, int pid, int bpos, ENetSocket &pongsock, ENet
         putint(p,clients[i]->state.state);      //State (Alive,Dead,Spawning,Lagged,Editing)
         uint ip = clients[i]->peer->address.host; // only 3 byte of the ip address (privacy protected)
         p.put((uchar*)&ip,3);
-        putint(p,clients[i]->state.damage); //Damage
+        putint(p,ismatch ? 0 : clients[i]->state.damage); //Damage
+        putint(p,ismatch ? 0 : clients[i]->state.shotdamage); //Shot with damage
 
         buf.dataLength = len + p.length();
         enet_socket_send(pongsock, &addr, &buf, 1);
