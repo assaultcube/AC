e73639461b519d65177d49c04dd2b91adaa69e01
aurhat <6187373+aurhat@users.noreply.github.com>
2021-10-20 23:01:35 +0200

    don't check collision during spectating in all player perspectives
    Related to 3c5012e

--- source/src/physics.cpp
+++ source/src/physics.cpp
@@ -366,7 +366,7 @@ void resizephysent(physent *pl, int moveres, int curtime, float min, float max)
     {
         pl->eyeheight += h;
         pl->o.z += h;
-        if(collide(pl) && !(pl != player1 && player1->isspectating() && player1->spectatemode==SM_FOLLOW1ST)) // don't check collision during spectating in 1st person view
+        if(collide(pl) && !(pl != player1 && player1->isspectating() && player1->spectatemode < SM_FLY))
         {
             pl->eyeheight -= h; // collided, revert mini-step
             pl->o.z -= h;
