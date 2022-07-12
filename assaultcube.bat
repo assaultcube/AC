e9d328cb27df690d00d3f49266649bdecd4a072d
flowtron <flowtron@users.noreply.github.com>
2022-07-12 11:40:10 +0200

    bump to 1.4

--- assaultcube.bat
+++ assaultcube.bat
@@ -1,1 +1,1 @@
-start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\My Games\AssaultCube\v1.3" --init %1 %2 %3 %4 %5
+start bin_win32\ac_client.exe "--home=?MYDOCUMENTS?\My Games\AssaultCube\v1.4" --init %1 %2 %3 %4 %5
