b56b1ee50e210b8bddc5890a959fa353cdf25b8c
Fabio <m.fabiojr.s@gmail.com>
2025-10-20 21:54:32 -0300

    feat: add configurable radar visibility for non-team game modes
    Changes:
    - Modified hideradar VARP
      * 0: Always show radar (default)
      * 1: Always hide radar
      * 2: Hide radar only in non-team modes
    - Replaced checkbox with slider in HUD settings menu
    - Added "Hide in non-team modes" option for better control
    
    This allows players to disable radar in FFA modes (deathmatch, pistol
    frenzy, etc.) while keeping it visible in team-based modes (TDM, CTF,
    etc.), providing more strategic gameplay options while maintaining
    the familiar default behavior.

--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -206,7 +206,7 @@ VARP(crosshairsize, 0, 15, 50);
 VARP(showstats, 0, 1, 2);
 VARP(crosshairfx, 0, 1, 3);
 VARP(crosshairteamsign, 0, 1, 1);
-VARP(hideradar, 0, 0, 1);
+VARP(hideradar, 0, 0, 2);
 VARP(hidecompass, 0, 0, 1);
 VARP(hideteam, 0, 0, 1);
 VARP(hideteamscorehud, 0, 0, 1);
@@ -947,7 +947,7 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwat
 
     if(!editmode)
     {
-        if((!hideradar || showmap) && player1->spectatemode <= SM_FLY) drawradar(p, w, h); // EDITMINIMAP: radar/showmap in editmode will be out-of-date
+        if((hideradar == 0 || (hideradar == 2 && m_teammode) || showmap) && player1->spectatemode <= SM_FLY) drawradar(p, w, h); // EDITMINIMAP: radar/showmap in editmode will be out-of-date
         glMatrixMode(GL_MODELVIEW);
         if(!hideteam && m_teammode) drawteamicons(w, h, is_spect);
         glMatrixMode(GL_PROJECTION);
